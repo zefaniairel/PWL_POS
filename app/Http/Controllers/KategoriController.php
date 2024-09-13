d051479057c7b422dce442905edb43dfb5dc94c5
zefania yizreel nefrit sindhoe <zefaniairel@gmail.com>
2024-09-14 00:48:24 +0700

    Pertemuan3-Praktikum5

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class KategoriController extends Controller
+{
+    public function index()
+    {
+        // $data = [
+        //     'kategori_kode' => 'SNK',
+        //     'kategori_nama' => 'Snack/Makanan RIngan',
+        //     'Created_at' => now()
+        // ];
+        // DB::table('m_kategori')->insert($data);
+        // return 'Insert data baru berhasil';
+
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
+        // return 'Update data behasil. Jumlah data yang diupdate: ' . $row. ' baris';
+
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
+        // return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row.' baris';    }
+
+        $data = DB::table('m_kategori')->get();
+        return view('kategori', ['data' => $data]);
+    }
+}
