761ded064d59a52c7e4ca0d0a60dd76c3fd5a343
zefania yizreel nefrit sindhoe <zefaniairel@gmail.com>
2024-09-18 08:57:19 +0700

    Pertemuan4-Praktikum2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,14 +10,23 @@ class UserController extends Controller
 {
     public function index()
     {
+
+        // JS 4 - PR2.1
+        $user = UserModel::findOr(1, ['username', 'nama'], function() {
+            abort(404);
+        }) ;
+        return view('user', ['data' => $user]);
+
+
         // tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password'=> Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // JS4 - Pr1
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password'=> Hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
         // UserModel::where('username', 'customer-1')->update($data); //update data user
 
@@ -31,7 +40,22 @@ class UserController extends Controller
         // UserModel::insert($data); // tambahkan data ke tabel m_user
         
         //coba akses model UserModel
-        $user = UserModel::all(); //ambill semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+        // $user = UserModel::all(); //ambill semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+
+        // JS4 - Pr2.1 , find
+        // $user = UserModel::find(1); 
+        // return view('user', ['data' => $user]);
+
+        // // JS4 - Pr2.1 , where
+        // $user = UserModel::where('level_id', 1)->first(); 
+        // return view('user', ['data' => $user]);
+
+        // JS4 - Pr2.1 , firstwhere
+        // $user = UserModel::firstWhere('level_id', 1); 
+        // return view('user', ['data' => $user]);
+
+
     }
+
 }
