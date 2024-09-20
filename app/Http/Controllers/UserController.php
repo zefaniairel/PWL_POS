6fd4be37caba50ecc93770c8b9f2c8a31fe16212
zefania yizreel nefrit sindhoe <zefaniairel@gmail.com>
2024-09-20 20:57:06 +0700

    Pertemuan4-Praktikum2.7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,11 +9,11 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     // JS4-Prak2.6
-    public function index()
-    {
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
-    }
+    // public function index()
+    // {
+    //     $user = UserModel::all();
+    //     return view('user', ['data' => $user]);
+    // }
 
     public function tambah()
     {
@@ -58,6 +58,12 @@ class UserController extends Controller
         $user->delete();
 
         return redirect('/user');
+    }
+
+    public function index()
+    {
+        $user = UserModel::with('level')->get();
+        dd($user);
     }
 
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -12,6 +12,8 @@
                 <td>Username</td>
                 <td>Nama</td>
                 <td>ID Level Pengguna</td>
+                <td>Kode Level</td>
+                <td>Nama Level</td>
                 <td>Aksi</td>
             </tr>
             @foreach ($data as $d)
@@ -20,6 +22,8 @@
                     <td>{{ $d->username }}</td>
                     <td>{{ $d->nama }}</td>
                     <td>{{ $d->level_id }}</td>
+                    <td>{{ $d->level->level_kode }} </td>
+                    <td>{{ $d->level->level_nama }} </td>
                     <td><a href="/PWL_POS/public/user/ubah/{{ $d->user_id }}">Ubah</a> 
                         <a href="/PWL_POS/public/user/hapus/{{ $d->user_id }}">Hapus</a></td>
                 </tr>   
