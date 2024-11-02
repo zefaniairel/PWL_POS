19b869010d2c310c5b1f9ee6e5d8738ffad8f40e
zefania yizreel nefrit sindhoe <zefaniairel@gmail.com>
2024-11-02 22:14:46 +0700

    Pertemuan10-Praktikum4

---
+++ app/Http/Controllers/Api/LevelController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\LevelModel;
+use Illuminate\Http\Request;
+
+class LevelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return LevelModel::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $level = LevelModel::create($request->all());
+        return response()->json($level,201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LevelModel $level)
+    {
+        return LevelModel::find($level);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LevelModel $level)
+    {
+        $level->update($request->all());
+        return LevelModel::find($level);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LevelModel $level)
+    {
+        $level->delete();
+
+        return response()->json([
+            'success'   => true,
+            'message'   => 'Data Terhapus',
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,11 +18,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
-Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 Route::middleware('auth:api')->get('/user', function (Request $request){
     return $request->user();
 });
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
